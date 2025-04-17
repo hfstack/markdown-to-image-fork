1b94708be36f375683b7ebc651cf2ce42d34e4c4
longfei.huang1 <longfei.huang1@geely.com>
2025-04-17 17:03:07 +0800

    feat: 多语言问题优化

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,16 +1,22 @@
+"use client"
+
 import * as React from 'react'
 import { useLocale } from 'next-intl'
-import { Link } from '../i18n/routing'
+import { Link, usePathname } from '../i18n/routing'
 
 export default function LanguageSwitcher() {
   const locale = useLocale()
+  const pathname = usePathname()
+  
+  console.log('LanguageSwitcher rendering, current locale:', locale);
+  console.log('Current pathname:', pathname);
   
   return (
     <div className="flex gap-4">
-      <Link href="/" locale="en" className={locale === 'en' ? 'font-bold' : ''}>
+      <Link href={pathname} locale="en" className={locale === 'en' ? 'font-bold' : ''}>
         English
       </Link>
-      <Link href="/" locale="zh" className={locale === 'zh' ? 'font-bold' : ''}>
+      <Link href={pathname} locale="zh" className={locale === 'zh' ? 'font-bold' : ''}>
         中文
       </Link>
     </div>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,28 @@
-import { Link } from '../i18n/routing'
+"use client"
+
+import { Link, usePathname } from '../i18n/routing'
+import { useTranslations, useLocale } from 'next-intl'
+import { useEffect } from 'react'
 
 export default function Navigation() {
+  const t = useTranslations('navigation')
+  const locale = useLocale()
+  const pathname = usePathname()
+  
+  // 添加useEffect以监听语言变化
+  useEffect(() => {
+    console.log('Navigation: locale changed to', locale);
+    console.log('Translated md2richTxt:', t('md2richTxt'));
+  }, [locale, t]);
+  
   return (
     <nav>
       <ul className="flex gap-4">
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/" locale={locale}>{t('home')}</Link>
+        </li>
+        <li>
+          <Link href="/md2richTxt" locale={locale}>{t('md2richTxt')}</Link>
         </li>
       </ul>
     </nav>
